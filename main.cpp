3bf723d1aa44b7cea81629089d544fae36af9b8f
BuildTools <if16b030@technikum-wien.at>
2017-05-26 15:43:46 +0200

    Added argument parsing and user input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
-#include <iostream>
+/* Copyright Alexander Dietrich und Selina Brinnich */
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <fstream>
+#include "connectionplan.h"
+
+int main(int argc, char *argv[]) {
+
+    // Check if filepath was entered as program argument
+    if(argc != 2){
+        std::cout << "Invalid parameters specified!" << std::endl;
+        return -1;
+    }
+
+    // Check if file exists
+    std::string filename = argv[1];
+    if(!std::ifstream(filename)){
+        std::cout << "Specified file does not exist or could not be read!" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Welcome to JourneyPlanner!" << std::endl;
+    ConnectionPlan* plan = new ConnectionPlan();
+
+    // Read connection plan from file
+    plan->readFromFile(filename);
+
+    std::string in = "", station1 = "", station2 = "";
+    do{
+        std::cout << "Press s for searching a path or any other character to exit program!" << std::endl;
+        std::cin >> in;
+        if(in.compare("s") == 0){
+            std::cout << "Where do you want to start your journey?";
+            std::cin >> station1;
+            std::cout << "What is your destination?";
+            std::cin >> station2;
+            plan->printPath(plan->getShortestPath(station1, station2));
+        }
+    }while(in.compare("s") == 0);
+
+    delete plan;
     return 0;
 }
